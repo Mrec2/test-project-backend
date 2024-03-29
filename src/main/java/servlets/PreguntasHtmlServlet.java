acb8b0fdafe6727ed35843ae23cea4554bb0c2a6
Juanal <34882607+Juanal85@users.noreply.github.com>
2024-03-29 16:28:36 +0100

    Added Servlets

---
+++ src/main/java/servlets/PreguntasHtmlServlet.java
@@ -0,0 +1,65 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javaBeans.GestorPreguntasHTML;
+import javaBeans.PreguntaTest;
+
+
+
+
+@WebServlet("/PreguntasHtmlServlet")
+public class PreguntasHtmlServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		System.out.println("Entra en Get del Servlet Css");
+		
+		procesar(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		System.out.println("Entra en Post del Servlet Css");
+		
+		procesar(request, response);
+		
+	}
+	
+	private void procesar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		try {
+		
+			GestorPreguntasHTML gestor = new GestorPreguntasHTML();
+	        List<PreguntaTest> preguntas = gestor.getPreguntas();
+	        Gson gson = new Gson();
+	        String json = gson.toJson(preguntas);
+	        
+	        System.out.println("Esto es lo que se envia " + json); 
+	        response.setContentType("application/json");
+	        response.setCharacterEncoding("UTF-8");
+	        response.getWriter().write(json);
+	    
+		} catch (IOException e) {
+	    
+			e.printStackTrace();
+	        
+	    }
+		
+	}
+
+}
